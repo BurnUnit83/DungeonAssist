6ea05a880b9bee7b6056e12a647c7e4f2c2571be
BurnUnit83 <59190640+BurnUnit83@users.noreply.github.com>
2021-06-03 15:32:51 -0700

    Update EdensGateInundationLevi.cs
    Adding diagnostic logging

--- Plugins/Data/EdensGateInundationLevi.cs
+++ Plugins/Data/EdensGateInundationLevi.cs
@@ -3,6 +3,12 @@ using Clio.Utilities;
 using ff14bot;
 using ff14bot.Behavior;
 using ff14bot.Managers;
+using System;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Media;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Threading.Tasks;
@@ -39,21 +45,26 @@ namespace DungeonAssist
 			HashSet<uint> TempCurrentRight = new HashSet<uint>() { 16333 };
              if (TempCurrentRight.IsCasting())
              {
+				Logging.Write(Colors.Aquamarine, $"Temporary Current Right Handling - Handling");
 				//SIDESTEP TOGGLE
 					 if (PluginManager.Plugins.Where(p => p.Plugin.Name == "SideStep" || p.Plugin.Name == "回避").Any())
 						{
 							var _plugin = PluginManager.Plugins.Where(p => p.Plugin.Name == "SideStep" || p.Plugin.Name == "回避").FirstOrDefault();
-							if (_plugin.Enabled == true) { _plugin.Enabled = false; }
+							if (_plugin.Enabled == true) 
+							{ _plugin.Enabled = false; 
+								Logging.Write(Colors.Aquamarine, $"SideStep Off!");
+							}
 						}
 				//SIDESTEP TOGGLE				
-                Vector3 _loc = new Vector3(81, 0, 112);
+                Logging.Write(Colors.Aquamarine, $"Temporary Current Right Handling - Moving to Coordinates");
+				Vector3 _loc = new Vector3(81, 0, 112);
 
                 while (Core.Me.Distance(_loc) > 1f)
                 {
                     await CommonTasks.MoveTo(_loc);
                     await Coroutine.Yield();
                 }
-
+				Logging.Write(Colors.Aquamarine, $"Temporary Current Right Handling - Sleeping");
                 await Coroutine.Sleep(3000);
 
                 if (ActionManager.IsSprintReady)
@@ -63,8 +74,8 @@ namespace DungeonAssist
                 }
                  Stopwatch sw = new Stopwatch();
                  sw.Start();
-				 //Logging.Write(Colors.Aquamarine, $"Ancient Flare Handling");
-                 
+				 
+                 Logging.Write(Colors.Aquamarine, $"Temporary Current Right Handling - Sleeping 2");
 				 await Coroutine.Sleep(1000);
                  await Coroutine.Yield();
              
@@ -73,21 +84,24 @@ namespace DungeonAssist
 					 if (PluginManager.Plugins.Where(p => p.Plugin.Name == "SideStep" || p.Plugin.Name == "回避").Any())
 						{
 							var _plugin = PluginManager.Plugins.Where(p => p.Plugin.Name == "SideStep" || p.Plugin.Name == "回避").FirstOrDefault();
-							if (_plugin.Enabled == false) { _plugin.Enabled = true; }
+							if (_plugin.Enabled == false) { _plugin.Enabled = true; Logging.Write(Colors.Aquamarine, $"Temporary Current Right Handling - Sidestep On");}
 						}
 				//SIDESTEP TOGGLE	
+				Logging.Write(Colors.Aquamarine, $"Temporary Current Right Handling - Complete");
              }
 			
 			 HashSet<uint> TempCurrentLeft = new HashSet<uint>() { 16334 };
              if (TempCurrentLeft.IsCasting())
              {
+				 Logging.Write(Colors.Aquamarine, $"Temporary Current Left Handling - Handling");
 				//SIDESTEP TOGGLE
 					 if (PluginManager.Plugins.Where(p => p.Plugin.Name == "SideStep" || p.Plugin.Name == "回避").Any())
 						{
 							var _plugin = PluginManager.Plugins.Where(p => p.Plugin.Name == "SideStep" || p.Plugin.Name == "回避").FirstOrDefault();
-							if (_plugin.Enabled == true) { _plugin.Enabled = false; }
+							if (_plugin.Enabled == true) { _plugin.Enabled = false; Logging.Write(Colors.Aquamarine, $"Temporary Current Left Handling - Sidestep Off!");}
 						}
-				//SIDESTEP TOGGLE	              
+				//SIDESTEP TOGGLE	        
+					Logging.Write(Colors.Aquamarine, $"Temporary Current Left Handling - Moving to Coordinates");
                 Vector3 _loc = new Vector3(87, 0, 82);
 
                 while (Core.Me.Distance(_loc) > 1f)
@@ -95,7 +109,7 @@ namespace DungeonAssist
                     await CommonTasks.MoveTo(_loc);
                     await Coroutine.Yield();
                 }
-
+				Logging.Write(Colors.Aquamarine, $"Temporary Current Left Handling - Sleeping 1");
                 await Coroutine.Sleep(3000);
 
                 if (ActionManager.IsSprintReady)
@@ -107,6 +121,7 @@ namespace DungeonAssist
                  sw.Start();
 				 //Logging.Write(Colors.Aquamarine, $"Ancient Flare Handling");
                  
+				 Logging.Write(Colors.Aquamarine, $"Temporary Current Left Handling - Sleeping 2");
 				 await Coroutine.Sleep(1000);
                  await Coroutine.Yield();
              
@@ -115,20 +130,23 @@ namespace DungeonAssist
 					 if (PluginManager.Plugins.Where(p => p.Plugin.Name == "SideStep" || p.Plugin.Name == "回避").Any())
 						{
 							var _plugin = PluginManager.Plugins.Where(p => p.Plugin.Name == "SideStep" || p.Plugin.Name == "回避").FirstOrDefault();
-							if (_plugin.Enabled == false) { _plugin.Enabled = true; }
+							if (_plugin.Enabled == false) { _plugin.Enabled = true; Logging.Write(Colors.Aquamarine, $"Temporary Current Left Handling - Sidestep On");}
 						}
 				//SIDESTEP TOGGLE	
+				Logging.Write(Colors.Aquamarine, $"Temporary Current Left Handling - Complete");
              }
 			 HashSet<uint> TidalWaveQuake = new HashSet<uint>() { 16337,16338,16339 };
              if (TidalWaveQuake.IsCasting())
              {
+				 Logging.Write(Colors.Aquamarine, $"Quale/Tidal Handling - Triggerered");
 				//SIDESTEP TOGGLE
 					 if (PluginManager.Plugins.Where(p => p.Plugin.Name == "SideStep" || p.Plugin.Name == "回避").Any())
 						{
 							var _plugin = PluginManager.Plugins.Where(p => p.Plugin.Name == "SideStep" || p.Plugin.Name == "回避").FirstOrDefault();
-							if (_plugin.Enabled == true) { _plugin.Enabled = false; }
+							if (_plugin.Enabled == true) { _plugin.Enabled = false; Logging.Write(Colors.Aquamarine, $"Quale/Tidal Handling - Sidestep Off");}
 						}
-				//SIDESTEP TOGGLE	                
+				//SIDESTEP TOGGLE	
+				Logging.Write(Colors.Aquamarine, $"Quale/Tidal Handling - Moving to Dead Center");
                 Vector3 _loc = new Vector3(99, 0, 99);
 
                 while (Core.Me.Distance(_loc) > 1f)
@@ -136,7 +154,7 @@ namespace DungeonAssist
                     await CommonTasks.MoveTo(_loc);
                     await Coroutine.Yield();
                 }
-
+				Logging.Write(Colors.Aquamarine, $"Quale/Tidal Handling - Sleeping 1");
                 await Coroutine.Sleep(3000);
 
                 if (ActionManager.IsSprintReady)
@@ -148,6 +166,7 @@ namespace DungeonAssist
                  sw.Start();
 				 //Logging.Write(Colors.Aquamarine, $"Ancient Flare Handling");
                  
+				 Logging.Write(Colors.Aquamarine, $"Quale/Tidal Handling - Sleeping 2");
 				 await Coroutine.Sleep(1000);
                  await Coroutine.Yield();
              
@@ -156,9 +175,10 @@ namespace DungeonAssist
 					 if (PluginManager.Plugins.Where(p => p.Plugin.Name == "SideStep" || p.Plugin.Name == "回避").Any())
 						{
 							var _plugin = PluginManager.Plugins.Where(p => p.Plugin.Name == "SideStep" || p.Plugin.Name == "回避").FirstOrDefault();
-							if (_plugin.Enabled == false) { _plugin.Enabled = true; }
+							if (_plugin.Enabled == false) { _plugin.Enabled = true; Logging.Write(Colors.Aquamarine, $"Quale/Tidal Handling - Sidestep On");}
 						}
 				//SIDESTEP TOGGLE	
+				Logging.Write(Colors.Aquamarine, $"Quale/Tidal Handling - Complete");
              }
 			 
 			
@@ -166,19 +186,23 @@ namespace DungeonAssist
 
             /// Default (缺省)
             if (Spells.IsCasting()) { Core.Me.ClearTarget();			//Logging.Write(Colors.Aquamarine, $"Default Spell Handling");
+					Logging.Write(Colors.Aquamarine, $"Generic Default Handling - Triggered");
 					while (Core.Me.Location.Distance2D(PartyManager.VisibleMembers.Where(x => !x.IsMe && x.BattleCharacter.IsAlive).FirstOrDefault().BattleCharacter.Location) > 0.5)
 					{
 						MovementManager.SetFacing(PartyManager.VisibleMembers.Where(x => !x.IsMe && x.BattleCharacter.IsAlive).FirstOrDefault().BattleCharacter.Location);
+						Logging.Write(Colors.Aquamarine, $"Generic Default Handling - Moving to Nearest Alive Party Character");
 						MovementManager.MoveForwardStart();
+						Logging.Write(Colors.Aquamarine, $"Generic Default Handling - Waiting");
 						await Coroutine.Sleep(100);
 						MovementManager.MoveStop();
 					}
+					Logging.Write(Colors.Aquamarine, $"Generic Default Handling - Complete");
 				//await Coroutine.Sleep(1000);
-                    await Coroutine.Yield();; }
+                    await Coroutine.Yield(); 
 
             /// SideStep (回避)
             //if (WorldManager.SubZoneId != 2996) { Helpers.BossIds.ToggleSideStep(new uint[] { 8210 }); } else { Helpers.BossIds.ToggleSideStep(); }
-
+			}
             return false;
         }
     }
