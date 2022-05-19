f9f2b7e98648e958177639aa92fc7c44a3b5c227
domesticwarlord86 <domesticwarlord86@gmail.com>
2022-05-19 06:44:41 -0500

    Removed catching death. It's going in Osiris

--- Data/ThePortaDecumana.cs
+++ Data/ThePortaDecumana.cs
@@ -120,17 +120,6 @@ namespace DungeonAssist
                 await MovementHelpers.GetClosestAlly.Follow();    
             }
 
-            if (Core.Me.CurrentHealth == 0)
-            {
-                Logging.WriteDiagnostic($"DungeonAssist cuaght death.");
-                await Coroutine.Wait(-1, () => (Core.Me.IsAlive));
-                Logging.WriteDiagnostic($"We are alive, loading profile...");
-                NeoProfileManager.Load(NeoProfileManager.CurrentProfile.Path);
-                NeoProfileManager.UpdateCurrentProfileBehavior();
-                await Coroutine.Sleep(5000);
-                return true;
-            }
-
             if (!Spells.IsCasting())
             {
                 if (!sidestepPlugin.Enabled)
